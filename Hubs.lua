3d7c6835b4b6377ed99aba430f7bed60bc37b711
K1ll3rN4m3 <84021555+K1ll3rN4m3@users.noreply.github.com>
2022-05-27 09:19:14 +0800

    Update Hubs.lua

--- Hubs.lua
+++ Hubs.lua
@@ -6,7 +6,8 @@ local tab2 = mainGUI.newTab("Blox Fruit");
 local tab3 = mainGUI.newTab("Brookhaven");
 local tab4 = mainGUI.newTab("Clicker Simulator")
 local tab5 = mainGUI.newTab("Weapon Fighting Simulator")
-local tab6 = mainGUI.newTab("Settings");
+local tab6 = mainGUI.newTab("Ragdoll Engine")
+local tab7 = mainGUI.newTab("Settings");
 
 -----------------------------------------------
 
@@ -150,42 +151,55 @@ end)
 
 -----------------------------------------------
 
-tab6.newTitle("Settings")
+tab6.newTitle("Ragdoll Engine Section");
 tab6.newDiv()
 
-tab6.newButton("ShiftLock (For Mobile Users)", function()
+tab6.newButton("CryptonicHub (PC, Mobile)", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/martinelcrac/cryptonichub/main/Ragdollengine.lua'))();
+end)
+
+tab6.newButton("Lz0x (PC, Mobile)", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/Htd0CisQ"))()
+end)
+
+-----------------------------------------------
+
+tab7.newTitle("Settings")
+tab7.newDiv()
+
+tab7.newButton("ShiftLock (For Mobile Users)", function()
     loadstring(game:HttpGet('https://pastebin.com/raw/WQ9NPeDS'))();
 end)
 
-tab6.newButton("Anti Fling", function()
+tab7.newButton("Anti Fling", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/RERTLsNp"))();
 end)
 
-tab6.newButton("Auto Translate", function()
+tab7.newButton("Auto Translate", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourLocalNzi/Language-translator-/main/Language%20translator", true))();
 end)
 
-tab6.newButton("Free Animations", function()
+tab7.newButton("Free Animations", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/Animation"))();
 end)
 
-tab6.newButton("Walk On Wall", function()
+tab7.newButton("Walk On Wall", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))();
 end)
 
-tab6.newButton("Fly", function()
+tab7.newButton("Fly", function()
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")();
 end)
 
-tab6.newButton("ESP", function()
+tab7.newButton("ESP", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/Esp"))();
 end)
 
-tab6.newButton("Keyboard", function()
+tab7.newButton("Keyboard", function()
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/manimcool21/Keyboard-FE/main/Protected%20(3).lua'),true))();
 end)
 
-tab6.newButton("Infinite Jump", function()
+tab7.newButton("Infinite Jump", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/Infinite%20Jump"))();
 end)
 
