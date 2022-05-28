8c734226dd9bdaf8246614f34defe8404bf0ca7b
K1ll3rN4m3 <84021555+K1ll3rN4m3@users.noreply.github.com>
2022-05-28 17:23:03 +0800

    Update Hubs.lua

--- Hubs.lua
+++ Hubs.lua
@@ -8,7 +8,8 @@ local tab4 = mainGUI.newTab("Clicker Simulator")
 local tab5 = mainGUI.newTab("Weapon Fighting Simulator")
 local tab6 = mainGUI.newTab("Ragdoll Engine")
 local tab7 = mainGUI.newTab("Legends Of Speed")
-local tab8 = mainGUI.newTab("Settings");
+local tab8 = mainGUI.newTab("Adopt Me")
+local tab9 = mainGUI.newTab("Settings");
 
 -----------------------------------------------
 
@@ -188,44 +189,61 @@ tab7.newButton("[This Hub Has No Name] (PC, Mobile)", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/LOS"))();
 end)
 
+tab7.newButton("V.G Hub (PC, Mobile)", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))();
+end)
+
 -----------------------------------------------
 
-tab8.newTitle("Settings")
+tab8.newTitle("Adopt Me Section");
 tab8.newDiv()
 
-tab8.newButton("ShiftLock (For Mobile Users)", function()
+tab8.newButton("Antartic Hub (PC, Mobile)", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JusticeValley/Antarctic-Hub/main/New.lua", true))();
+end)
+
+tab8.newButton("V.G Hub (PC, Mobile)", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))();
+end)
+
+-----------------------------------------------
+
+tab9.newTitle("Settings")
+tab9.newDiv()
+
+tab9.newButton("ShiftLock (For Mobile Users)", function()
     loadstring(game:HttpGet('https://pastebin.com/raw/WQ9NPeDS'))();
 end)
 
-tab8.newButton("Anti Fling", function()
+tab9.newButton("Anti Fling", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/RERTLsNp"))();
 end)
 
-tab8.newButton("Auto Translate", function()
+tab9.newButton("Auto Translate", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourLocalNzi/Language-translator-/main/Language%20translator", true))();
 end)
 
-tab8.newButton("Free Animations", function()
+tab9.newButton("Free Animations", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/Animation"))();
 end)
 
-tab8.newButton("Walk On Wall", function()
+tab9.newButton("Walk On Wall", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))();
 end)
 
-tab8.newButton("Fly", function()
+tab9.newButton("Fly", function()
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")();
 end)
 
-tab8.newButton("ESP", function()
+tab9.newButton("ESP", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/Esp"))();
 end)
 
-tab8.newButton("Keyboard", function()
+tab9.newButton("Keyboard", function()
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/manimcool21/Keyboard-FE/main/Protected%20(3).lua'),true))();
 end)
 
-tab8.newButton("Infinite Jump", function()
+tab9.newButton("Infinite Jump", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/Scripts/main/Infinite%20Jump"))();
 end)
 
