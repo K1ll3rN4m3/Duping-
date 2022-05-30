a13f5e7030f54b2435fc620379c7738a46a4de56
K1ll3rN4m3 <84021555+K1ll3rN4m3@users.noreply.github.com>
2022-05-30 21:07:47 +0800

    Update Updates.lua

--- Updates.lua
+++ Updates.lua
@@ -12,7 +12,7 @@ Player.Chatted:connect(function(cht)
         print("[-] TikTok: @tph_auraph")
         
         print("------------------ Updates ------------------")
-        print("[+] Added: Adopt Me")
+        print("[+] Added: Zombie Attack")
         
         print("------------------ More Soon -----------------")
     end
